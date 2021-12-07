0d7d8fe874ada3abe3bbf9f458597772e6f1f39e
ahmeturgancim <aahmet.272@gmail.com>
2021-12-07 13:25:17 +0300

    created new component and  news datas

---
+++ src/app/config/newsApi.js
@@ -0,0 +1,5 @@
+const apiKey = 'f13d957b08ef4d75881a5faeccfccb1a';
+const topHeadlinesUrl =
+  'https://newsapi.org/v2/top-headlines?country=tr&apiKey=' + apiKey;
+
+export { apiKey, topHeadlinesUrl };
---
+++ src/components/news-item/news-item.js
@@ -0,0 +1,38 @@
+const newItem = document.createElement("template");
+newItem.innerHTML = `
+<div class="article-wrapper">
+    <h1 class="title"></h1>
+   <!-- <p class="content"></p>
+    <div class="description"></div>
+    <div class="publishedAt"></div>
+    <div class="source"></div>
+    <a target="_blank" class="url">IMDb</a>
+    <img src="" class="urlToImage" alt="" height="100"> -->
+</div>
+`;
+class NewsItem extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" });
+        this.shadowRoot.appendChild(newItem.content.cloneNode(true));
+        setTimeout(() => {
+            this.shadowRoot.querySelector(".article-wrapper .title").innerHTML = this.getAttribute("title");
+
+            //    this.shadowRoot.querySelector(".article-wrapper .content").innerHTML = this.getAttribute("content");
+            //    this.shadowRoot.querySelector(".article-wrapper .description").innerHTML = this.getAttribute("description").substring(0, this.getAttribute("description").indexOf('...'));
+            //    this.shadowRoot.querySelector(".article-wrapper .publishedAt").innerHTML = this.getAttribute("publishedAt");
+            //    this.shadowRoot.querySelector(".article-wrapper .source").innerHTML = this.getAttribute("source");
+            //    this.shadowRoot
+            //    this.shadowRoot.querySelector(".article-wrapper .url")
+            //    .setAttribute(
+            //      "href",
+            //      this.getAttribute("url")
+            //    );
+            //    this.shadowRoot.querySelector(".article-wrapper .urlToImage").src = this.getAttribute("urlToImage");
+            
+        }, 100);
+
+    }
+}
+
+window.customElements.define("news-item", NewsItem);
