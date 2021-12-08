b034c3e459f0c9835f911788e954e623a49a3b43
ahmeturgancim <aahmet.272@gmail.com>
2021-12-08 15:41:44 +0300

    tasarım düzenşendi

--- src/components/news-item/news-item.js
+++ src/components/news-item/news-item.js
@@ -1,42 +1,57 @@
 const newItem = document.createElement("template");
 newItem.innerHTML = `
-<style>
-@import url('http://${location.host}/src/components/news-item/news-item.css')
-</style>
-<div class="article-wrapper">
-    <div class="fill">
-        <img src="" class="urlToImage" alt="" height="250">
-    </div>
-    <div class="new-header">
-        <h1 class="title"></h1>
-        <span class="publishedAt"></span>
-        <span class="author"></span>
-    </div>
-    <p class="description">
-        <a href="" class="url"> Devamını oku...</a>
-    </p>
-</div>
+    <style>
+      @import url("http://${location.host}/src/components/news-item/news-item.css");
+    </style>
+
+    <div class="article-wrapper card" >
+      <div class="wrapper">
+        <div class="publishedAt">
+        </div>
+        <div class="data">
+          <div class="content">
+            <span class="author"></span>
+               <h1 class="title"></h1>
+            <p class="description"></p>
+          </div>
+        </div>
+      </div>
+  </div>
 
 `;
 class NewsItem extends HTMLElement {
-    constructor() {
-        super();
-        this.attachShadow({ mode: "open" });
-        this.shadowRoot.appendChild(newItem.content.cloneNode(true));
-        setTimeout(() => {
-            this.shadowRoot.querySelector(".article-wrapper .new-header .title").innerHTML = this.getAttribute("title");
-            this.shadowRoot.querySelector(".article-wrapper .description").innerHTML = this.getAttribute("description");
-            this.shadowRoot.querySelector(".article-wrapper .publishedAt").innerHTML = this.getAttribute("publishedAt");
-            this.shadowRoot.querySelector(".article-wrapper .author").innerHTML = this.getAttribute('author');
-            // this.shadowRoot.querySelector(".article-wrapper .description .url")
-            // .setAttribute(
-            //     "href",
-            //     this.getAttribute("url")
-            //   );
-            this.shadowRoot.querySelector(".article-wrapper .fill .urlToImage").src = this.getAttribute("urlToImage");
-        }, 100);
-
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(newItem.content.cloneNode(true));
+    setTimeout(() => {
+      this.shadowRoot.querySelector("div .publishedAt").innerHTML = (new Date(this.getAttribute("publishedAt"))).toLocaleTimeString(); //new Date(Date.parse(this.getAttribute("publishedAt")));
+      this.shadowRoot.querySelector("div .title").innerHTML = this.getAttribute("title");
+      this.shadowRoot.querySelector("div .description").innerHTML = this.getAttribute("description");
+      this.shadowRoot.querySelector("div .wrapper").style.background = `url(${this.getAttribute("urlToImage")}) center / cover no-repeat`;      
+    }, 100);
+  }
+    goDetail() {
+      window.open(
+        this.getAttribute("url"),
+        '_blank'
+      );
     }
+
+  connectedCallback() {
+    this.shadowRoot
+      .querySelector(".card")
+      .addEventListener("click", () => this.goDetail());
+  }
+
+  disconnectedCallback() {
+    this.shadowRoot
+      .querySelector(".card")
+      .removeEventListener("click", () => this.goDetail());
+  }
+ 
 }
+
+
 
 window.customElements.define("news-item", NewsItem);
